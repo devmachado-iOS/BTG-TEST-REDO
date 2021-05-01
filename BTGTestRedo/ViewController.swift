1b46e973b6aaf0a88450787293ed0178a197d57e
Carlos Machado <ctl_cmachado@uolinc.com>
2021-05-01 16:04:39 -0300

    Initial Commit

---
+++ BTGTestRedo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BTGTestRedo
+//
+//  Created by Carlos Henrique Veloso Machado on 01/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
